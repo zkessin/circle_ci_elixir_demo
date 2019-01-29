1e6ad6d8c167089a549aa4fc81e032c38c79fbd7
Zachary Kessin <zkessin@gmail.com>
2019-01-29 09:56:41 +0200

    fail on false

--- test/test_app_test.exs
+++ test/test_app_test.exs
@@ -2,6 +2,10 @@ defmodule TestAppTest do
   use ExUnit.Case
   doctest TestApp
 
+  test "will fail" do
+    assert false
+  end
+
   test "greets the world" do
     assert TestApp.hello() == :world
   end
